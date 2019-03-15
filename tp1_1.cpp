403626e93ba4da1f63fc19e568fc69590e4e3503
AngelV1107 <angelleandro@live.com>
2019-03-15 13:30:56 -0300

    Resolucion tp1_1

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,3 +1,20 @@
-nada
-la nada misma
-La nada 2: la venganza
+#include <stdio.h>
+
+int main () {
+
+	int var, *punt1;
+
+	var = 10;
+	punt1 = &var;
+
+
+	printf("Valor de la variable: %d\n", var);
+	printf("Contenido del puntero: %d\n", *punt1);
+	printf("Direccion almacenada por el puntero: %p\n", punt1);
+	printf("Direccion de memoria de la variable: %p\n", &var);
+	printf("Direccion de memoria del puntero: %p\n", &punt1);
+	printf("Tamanio de memoria utilizado por la variable: %d bytes\n", sizeof(var));
+	
+	puts("FIN");
+	return 0;
+}
